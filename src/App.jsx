5fd9702208c2123de5fcf9121d6dba74bf467767
Camila Cristina <criscamila09@gmail.com>
2022-08-12 14:39:37 -0300

    about component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { About } from './components/About/About'
 import { Home } from './components/Home/Home'
 import { Login } from './components/Login/Login'
 
@@ -5,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <Home />
+      <About />
     </div>
   )
 }
---
+++ src/components/About/About.jsx
@@ -0,0 +1,24 @@
+import styles from "./About.module.css";
+
+export function About() {
+  return (
+    <section className={styles.about}>
+      <header>
+        <h2>Quem somos</h2>
+      </header>
+
+      <div className={styles.content}>
+        <p>
+          Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+          sint. Velit officia consequat duis enim. Amet minim mollit non
+          deserunt ullamco est sit aliqua dolor do amet sint. Velit officia
+          consequat duis enim. Amet minim mollit non deserunt ullamco est sit
+          aliqua dolor do amet sint. Velit officia consequat duis enim. Amet
+          minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.
+          Velit officia consequat duis enim. Amet minim mollit non deserunt
+          ullamco est sit aliqua dolor do amet sint.
+        </p>
+      </div>
+    </section>
+  );
+}
