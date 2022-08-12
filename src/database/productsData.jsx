b64382ca523fe407daf01aadefb2837b867466f9
Camila Cristina <criscamila09@gmail.com>
2022-08-12 15:20:47 -0300

    database

---
+++ src/database/productsData.jsx
@@ -0,0 +1,110 @@
+export const productsData = [
+  {
+    id: '0',
+    img: './src/database/productsImg/blusa-decote-listrada.jpg',
+    productName: 'Blusa Decote V Listrada',
+    productPrice: 'Blusa Decote V Listrada',
+    productDivision: '6x de R$ 15,00',
+    hasDiscount: true,
+    productType: 'blouse'
+  },
+  {
+    id: '1',
+    img: './src/database/productsImg/calca-jeans-personalizada.jpg',
+    productName: 'Calça Jeans Estilizada',
+    productPrice: 'R$ 150,00',
+    productDivision: '6x de R$ 25,00',
+    hasDiscount: false,
+    productType: 'jeans'
+  },
+  {
+    id: '2',
+    img: './src/database/productsImg/conjunto-azul.jpg',
+    productName: 'Conjunto Marinho',
+    productPrice: 'R$ 150,00',
+    productDivision: '6x de R$ 25,00',
+    hasDiscount: false,
+    productType: 'conjunct'
+  },
+  {
+    id: '3',
+    img: './src/database/productsImg/conjunto-branco-alfaiataria',
+    productName: 'Conjunto Alfaiataria Branco',
+    productPrice: 'R$ 240,00',
+    productDivision: '6x de R$ 40,00',
+    hasDiscount: true,
+    productType: 'conjunct'
+  },
+  {
+    id: '4',
+    img: './src/database/productsImg/conjunto-moletom-amarelo.jpg',
+    productName: 'Conjunto Moletom Amarelo',
+    productPrice: 'R$ 180,00',
+    productDivision: '6x de R$ 30,00',
+    hasDiscount: false,
+    productType: 'conjunct'
+  },
+  {
+    id: '5',
+    img: './src/database/productsImg/conjunto-terno-preto.jpg',
+    productName: 'Conjunto Terno Preto',
+    productPrice: 'R$ 210,00',
+    productDivision: '6x de R$ 35,00',
+    hasDiscount: false,
+    productType: 'conjunct'
+  },
+  {
+    id: '6',
+    img: './src/database/productsImg/t-shirt-branca.jpg',
+    productName: 'T-shirt Branca Leave the Road',
+    productPrice: 'R$ 60,00',
+    productDivision: '6x de R$ 10,00',
+    hasDiscount: true,
+    productType: 't-shirt'
+  },
+  {
+    id: '7',
+    img: './src/database/productsImg/t-shirt-preta.jpg',
+    productName: 'T-shirt Esqueleto Preta',
+    productPrice: 'R$ 60,00',
+    productDivision: '6x de R$ 10,00',
+    hasDiscount: false,
+    productType: 't-shirt'
+  },
+  {
+    id: '8',
+    img: './src/database/productsImg/vestido-floral-azul',
+    productName: 'Vestido Floral Verão Azul',
+    productPrice: 'R$ 90,00',
+    productDivision: '6x de R$ 15,00',
+    hasDiscount: false,
+    productType: 'dress'
+  },
+  {
+    id: '9',
+    img: './src/database/productsImg/vestido-floral-vermelho.jpg',
+    productName: 'Vestido Floral Vermelho',
+    productPrice: 'R$ 150,00',
+    productDivision: '6x de R$ 25,00',
+    hasDiscount: true,
+    productType: 'dress'
+  },
+  {
+    id: '10',
+    img: './src/database/productsImg/vestido-ombro-azul.jpg',
+    productName: 'Vestido Verão Ombro a ombro',
+    productPrice: 'R$ 120,00',
+    productDivision: '6x de R$ 20,00',
+    hasDiscount: false,
+    productType: 'dress'
+  },
+  {
+    id: '11',
+    img: './src/database/productsImg/vestido-verao-branco.jpg',
+    productName: 'Vestido Verão Branco',
+    productPrice: 'R$ 90,00',
+    productDivision: '6x de R$ 15,00',
+    hasDiscount: true,
+    productType: 'dress'
+  }
+]
