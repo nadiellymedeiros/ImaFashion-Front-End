f218f24cf87a4e79f65a21558abcd0fca98bc528
Camila Cristina <criscamila09@gmail.com>
2022-08-12 13:16:53 -0300

    global font-size

--- src/components/Home/Stat.jsx
+++ src/components/Home/Stat.jsx
@@ -3,7 +3,7 @@ import styles from './Stat.module.css'
 export function Stat({ tittle, paragraph }) {
   return (
     <div className={styles.Stat}>
-      <h3>{tittle}</h3>
+      <h1>{tittle}</h1>
       <p>{paragraph}</p>
     </div>
   )
