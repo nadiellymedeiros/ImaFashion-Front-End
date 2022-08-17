3ad091076be4ea4e3c08ef5662f21d70e6353647
mirrageelpho <ramsolucoes@gmail.com>
2022-08-16 21:26:17 -0300

    removendo subtitulo na pagina de login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,7 +2,6 @@ export function Login() {
   return (
     <div>
       <h1>Login</h1>
-      <h2>Colaboradores</h2>
       <form>
         <input type="text" placeholder="E-mail"></input>
       </form>
