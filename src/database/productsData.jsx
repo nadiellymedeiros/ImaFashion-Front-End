f7c31ff771d3072256a28f1538466e27804b91bf
Camila Cristina <criscamila09@gmail.com>
2022-08-28 18:24:34 -0300

    product img src

--- src/database/productsData.jsx
+++ src/database/productsData.jsx
@@ -1,125 +1,125 @@
 export const productsData = [
   {
     id: '0',
-    img: '/src/database/productsImg/blusa-decote-listrada.jpg',
+    img: './src/database/productsImg/blusa-decote-listrada.jpg',
     productName: 'Blusa Decote V Listrada',
     productPrice: 90.0,
     productDivision: '6x de R$ 15,00',
     hasDiscount: true,
     productType: 'blouse',
-    size: ["P","M","G"],
+    size: ['P', 'M', 'G'],
     qty: 1
   },
   {
     id: '1',
-    img: '/src/database/productsImg/calca-jeans-personalizada.jpg',
+    img: './src/database/productsImg/calca-jeans-personalizada.jpg',
     productName: 'Calça Jeans Estilizada',
     productPrice: 150.0,
     productDivision: '6x de R$ 25,00',
     hasDiscount: false,
     productType: 'jeans',
-    size: ["P","M","G"],
+    size: ['P', 'M', 'G'],
     qty: 1
   },
   {
     id: '2',
-    img: '/src/database/productsImg/conjunto-azul.jpg',
+    img: './src/database/productsImg/conjunto-azul.jpg',
     productName: 'Conjunto Marinho',
     productPrice: 150.0,
     productDivision: '6x de R$ 25,00',
     hasDiscount: false,
     productType: 'conjunct',
-    size: ["P","M","G"],
+    size: ['P', 'M', 'G'],
     qty: 1
   },
   {
     id: '3',
-    img: '/src/database/productsImg/conjunto-branco-alfaiataria.jpg',
+    img: './src/database/productsImg/conjunto-branco-alfaiataria.jpg',
     productName: 'Conjunto Alfaiataria Branco',
     productPrice: 240.0,
     productDivision: '6x de R$ 40,00',
     hasDiscount: true,
     productType: 'conjunct',
-    size: ["P","M","G"]
+    size: ['P', 'M', 'G']
   },
   {
     id: '4',
-    img: '/src/database/productsImg/conjunto-moletom-amarelo.jpg',
+    img: './src/database/productsImg/conjunto-moletom-amarelo.jpg',
     productName: 'Conjunto Moletom Amarelo',
     productPrice: 180.0,
     productDivision: '6x de R$ 30,00',
     hasDiscount: false,
     productType: 'conjunct',
-    size: ["P","M","G"]
+    size: ['P', 'M', 'G']
   },
   {
     id: '5',
-    img: '/src/database/productsImg/conjunto-terno-preto.jpg',
+    img: './src/database/productsImg/conjunto-terno-preto.jpg',
     productName: 'Conjunto Terno Preto',
     productPrice: 210.0,
     productDivision: '6x de R$ 35,00',
     hasDiscount: false,
     productType: 'conjunct',
-    size: ["P","M","G"]
+    size: ['P', 'M', 'G']
   },
   {
     id: '6',
-    img: '/src/database/productsImg/t-shirt-branca.jpg',
+    img: './src/database/productsImg/t-shirt-branca.jpg',
     productName: 'T-shirt Branca Leave the Road',
     productPrice: 60.0,
     productDivision: '6x de R$ 10,00',
     hasDiscount: true,
     productType: 't-shirt',
-    size: ["P","M","G"]
+    size: ['P', 'M', 'G']
   },
   {
     id: '7',
-    img: '/src/database/productsImg/t-shirt-preta.jpg',
+    img: './src/database/productsImg/t-shirt-preta.jpg',
     productName: 'T-shirt Esqueleto Preta',
     productPrice: 60.0,
     productDivision: '6x de R$ 10,00',
     hasDiscount: false,
     productType: 't-shirt',
-    size: ["P","M","G"]
+    size: ['P', 'M', 'G']
   },
   {
     id: '8',
-    img: '/src/database/productsImg/vestido-floral-azul.jpg',
+    img: './src/database/productsImg/vestido-floral-azul.jpg',
     productName: 'Vestido Floral Verão Azul',
     productPrice: 90.0,
     productDivision: '6x de R$ 15,00',
     hasDiscount: false,
     productType: 'dress',
-    size: ["P","M","G"]
+    size: ['P', 'M', 'G']
   },
   {
     id: '9',
-    img: '/src/database/productsImg/vestido-floral-vermelho.jpg',
+    img: './src/database/productsImg/vestido-floral-vermelho.jpg',
     productName: 'Vestido Floral Vermelho',
     productPrice: 150.0,
     productDivision: '6x de R$ 25,00',
     hasDiscount: true,
     productType: 'dress',
-    size: ["P","M","G"]
+    size: ['P', 'M', 'G']
   },
   {
     id: '10',
-    img: '/src/database/productsImg/vestido-ombro-azul.jpg',
+    img: './src/database/productsImg/vestido-ombro-azul.jpg',
     productName: 'Vestido Verão Ombro a ombro',
     productPrice: 120.0,
     productDivision: '6x de R$ 20,00',
     hasDiscount: false,
     productType: 'dress',
-    size: ["P","M","G"]
+    size: ['P', 'M', 'G']
   },
   {
     id: '11',
-    img: '/src/database/productsImg/vestido-verao-branco.jpg',
+    img: './src/database/productsImg/vestido-verao-branco.jpg',
     productName: 'Vestido Verão Branco',
     productPrice: 90.0,
     productDivision: '6x de R$ 15,00',
     hasDiscount: true,
     productType: 'dress',
-    size: ["P","M","G"]
+    size: ['P', 'M', 'G']
   }
 ]
