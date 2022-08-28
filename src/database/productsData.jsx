eae1f0ed48cd0d7f0a5a21ec92aa20cec17df4a7
Camila Cristina <criscamila09@gmail.com>
2022-08-28 19:22:29 -0300

    fix images

--- src/database/productsData.jsx
+++ src/database/productsData.jsx
@@ -1,7 +1,7 @@
 export const productsData = [
   {
     id: '0',
-    img: './src/database/productsImg/blusa-decote-listrada.jpg',
+    img: 'https://images.unsplash.com/photo-1533399710066-c33de66fe6bb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
     productName: 'Blusa Decote V Listrada',
     productPrice: 90.0,
     productDivision: '6x de R$ 15,00',
@@ -12,7 +12,7 @@ export const productsData = [
   },
   {
     id: '1',
-    img: './src/database/productsImg/calca-jeans-personalizada.jpg',
+    img: 'https://images.unsplash.com/photo-1541099649105-f69ad21f3246?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
     productName: 'Calça Jeans Estilizada',
     productPrice: 150.0,
     productDivision: '6x de R$ 25,00',
@@ -23,7 +23,7 @@ export const productsData = [
   },
   {
     id: '2',
-    img: './src/database/productsImg/conjunto-azul.jpg',
+    img: 'https://images.unsplash.com/photo-1495385794356-15371f348c31?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=370&q=80',
     productName: 'Conjunto Marinho',
     productPrice: 150.0,
     productDivision: '6x de R$ 25,00',
@@ -34,10 +34,10 @@ export const productsData = [
   },
   {
     id: '3',
-    img: './src/database/productsImg/conjunto-branco-alfaiataria.jpg',
-    productName: 'Conjunto Alfaiataria Branco',
-    productPrice: 240.0,
-    productDivision: '6x de R$ 40,00',
+    img: 'https://images.unsplash.com/photo-1561932850-f13404855e53?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=363&q=80',
+    productName: 'Saia Verão Amarela',
+    productPrice: 84.0,
+    productDivision: '6x de R$ 14,00',
     hasDiscount: true,
     productType: 'conjunct',
     size: ['P', 'M', 'G']
@@ -64,7 +64,7 @@ export const productsData = [
   },
   {
     id: '6',
-    img: './src/database/productsImg/t-shirt-branca.jpg',
+    img: 'https://images.unsplash.com/photo-1554568218-0f1715e72254?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
     productName: 'T-shirt Branca Leave the Road',
     productPrice: 60.0,
     productDivision: '6x de R$ 10,00',
@@ -74,7 +74,7 @@ export const productsData = [
   },
   {
     id: '7',
-    img: './src/database/productsImg/t-shirt-preta.jpg',
+    img: 'https://images.unsplash.com/photo-1503342217505-b0a15ec3261c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
     productName: 'T-shirt Esqueleto Preta',
     productPrice: 60.0,
     productDivision: '6x de R$ 10,00',
@@ -84,7 +84,7 @@ export const productsData = [
   },
   {
     id: '8',
-    img: './src/database/productsImg/vestido-floral-azul.jpg',
+    img: 'https://images.unsplash.com/photo-1563178406-4cdc2923acbc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=396&q=80',
     productName: 'Vestido Floral Verão Azul',
     productPrice: 90.0,
     productDivision: '6x de R$ 15,00',
@@ -104,7 +104,7 @@ export const productsData = [
   },
   {
     id: '10',
-    img: './src/database/productsImg/vestido-ombro-azul.jpg',
+    img: 'https://images.unsplash.com/photo-1496217590455-aa63a8350eea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
     productName: 'Vestido Verão Ombro a ombro',
     productPrice: 120.0,
     productDivision: '6x de R$ 20,00',
@@ -114,10 +114,40 @@ export const productsData = [
   },
   {
     id: '11',
-    img: './src/database/productsImg/vestido-verao-branco.jpg',
+    img: 'https://images.unsplash.com/photo-1515372039744-b8f02a3ae446?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80',
     productName: 'Vestido Verão Branco',
     productPrice: 90.0,
     productDivision: '6x de R$ 15,00',
+    hasDiscount: true,
+    productType: 'dress',
+    size: ['P', 'M', 'G']
+  },
+  {
+    id: '12',
+    img: 'https://images.unsplash.com/photo-1608234807905-4466023792f5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80',
+    productName: 'Blusa Manga Longa Branca',
+    productPrice: 90.0,
+    productDivision: '6x de R$ 15,00',
+    hasDiscount: true,
+    productType: 'blouse',
+    size: ['P', 'M', 'G']
+  },
+  {
+    id: '13',
+    img: 'https://images.unsplash.com/photo-1604840255993-65208037a12a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+    productName: 'Vestido Mid Estampado',
+    productPrice: 120.0,
+    productDivision: '6x de R$ 20,00',
+    hasDiscount: true,
+    productType: 'dress',
+    size: ['P', 'M', 'G']
+  },
+  {
+    id: '14',
+    img: 'https://img.freepik.com/fotos-gratis/jovem-mulher-com-roupa-de-verao-fora-do-cafe_1303-16103.jpg?w=360&t=st=1661724917~exp=1661725517~hmac=e6e764a9eca1c47b9ae082b29a5ac810e9f0fd8cbb95138a73fd9cb06e2a27cc',
+    productName: 'Vestido Verde Militar de Botões',
+    productPrice: 150.0,
+    productDivision: '6x de R$ 25,00',
     hasDiscount: true,
     productType: 'dress',
     size: ['P', 'M', 'G']
